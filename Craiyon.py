d95db081712e4f13577081d3e69ee3223fb62527
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-01-13 11:44:37 +0100

    Update Craiyon.py

--- Craiyon.py
+++ Craiyon.py
@@ -4,11 +4,15 @@ from io import BytesIO
 import streamlit as st
 import translators.server as tts
 import base64
+
 def Generate(request):
-    generator = Craiyon()
-    result = generator.generate(request) 
-    images = result.images
-    return images
+	try:
+		generator = Craiyon()
+		result = generator.generate(request) 
+		images = result.images
+		return images
+	except:
+		return "Error"
 
 generator = Craiyon()
 
@@ -33,30 +37,46 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
-request = st.text_input("Sono in grado di disegnare tutto ciò che vuoi, se non ci credi provami🖌🤖","Dimmi solo cosa disegnare e lo  farò!")
+request = st.text_input("Sono in grado di disegnare tutto ciò che vuoi, se non ci credi provami🖌🤖","Un leone che suona il violino")
 
-st.markdown("<small>🤖Esempi : [Una bicletta sulla Luna] , [Un gatto chr gioca a calcio] , [Una mela dentro una galassia] , [Una borsa Rossa per eventi eleganti]</small><br>", unsafe_allow_html=True)
+with st.expander("Esempi di input da dare 🎨"):
+	st.markdown("🤖Esempi : \n- Una bicletta sulla Luna \n- Un gatto che gioca a calcio \n- Una mela dentro una galassia \n- Una borsa Rossa per eventi eleganti  \n-Un leone che suona il violino  \n-Un'auto che corre su una strada di ghiaccio  \n-Un topo che gioca a golf  \n-Un computer che dipinge quadri  \n-Una farfalla che gioca a scacchi  \n-Un uomo che cammina su un tappeto volante  \n 🤗 Non scordati di condividere gli output e il nostro sito con i tuoi amici o colleghi 🤗")
 
 cola , colb, colc = st.columns(3)
 
 
 if colb.button("Disegna le mie immagini 🖌"):
-	with st.spinner("🧑‍🎨 Attendi un attimo stiamo rapendo diversi artisti... ( circa 40 secondi ) 🧑‍🎨"):
+	with st.spinner("🧑‍🎨 Attendi un attimo ⏳ stiamo rapendo diversi artisti... ( circa 40 secondi ) 🧑‍🎨"):
 		new_request = tts.google(request, from_language="it", to_language="en")
 		image_files = Generate(new_request)
-		col1, col2, col3 = st.columns(3)
-		with col1:
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[0].encode("utf-8")))))
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[1].encode("utf-8")))))
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[2].encode("utf-8")))))
-		with col2:
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[3].encode("utf-8")))))
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[4].encode("utf-8")))))
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[5].encode("utf-8")))))
-		with col3:
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[6].encode("utf-8")))))
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[7].encode("utf-8")))))
-			st.image(Image.open(BytesIO(base64.decodebytes(image_files[8].encode("utf-8")))))
+		if image_files != "Error":
+			col1, col2, col3 = st.columns(3)
+			with col1:
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[0].encode("utf-8")))))
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[1].encode("utf-8")))))
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[2].encode("utf-8")))))
+			with col2:
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[3].encode("utf-8")))))
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[4].encode("utf-8")))))
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[5].encode("utf-8")))))
+			with col3:
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[6].encode("utf-8")))))
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[7].encode("utf-8")))))
+				st.image(Image.open(BytesIO(base64.decodebytes(image_files[8].encode("utf-8")))))
+			st.balloons()
+			st.info(" ☑️ Per scaricare le immagini clicca con il tasto destro del mouse e seleziona 'Salva immagine come...' ")
+			st.success("🤖 Ecco le tue immagini, non sono meravigliose? ")
+			st.warning("🤖 Se non ti piacciono, prova a cambiare input ")
+			#appendi il nuovo input nel file input.txt
+			with open("input.txt", "a") as file:
+				file.write(new_request+"\n")
+		else:
+			st.error("🤖Sembra che ci sia stato un errore, riprova più tardi🤖")
 
-
+with st.epander("👨‍👨‍👦‍👦 Lasciati ispirare dagli altri utenti"):
+	#leggi il file input.txt riga per riga e fai un markdown 
+	st.text("👨‍👨‍👦‍👦 Ecco alcuni esempi di input che altri utenti hanno dato:")
+	with open("input.txt", "r") as file:
+		for line in file:
+			st.markdown("\n- "+line)
 st.caption(" [© Intelligenza Artificiale Italia](https://www.intelligenzaartificialeitalia.net/)")
