6d684f3db50208d33af23d6a4999251beb8d20e1
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-01-13 10:06:32 +0100

    Update Craiyon.py

--- Craiyon.py
+++ Craiyon.py
@@ -2,6 +2,7 @@ from craiyon import Craiyon
 from PIL import Image 
 from io import BytesIO
 import streamlit as st
+import translators.server as tts
 import base64
 def Generate(request):
     generator = Craiyon()
@@ -14,7 +15,9 @@ st.title("Genera Immagini con l'IA")
 st.header("Metti alla prova la mia IA generando immagini")
 request = st.text_input("Sono in grado di disegnare tutto. Vuoi mettermi alla prova?","Dimmi solo cosa disegnare e lo farò!")
 if st.button("Draw"):
-    image_files = Generate(request)
+    new_request = tts.google(request, from_language="it", to_language="en")
+    print("italiano : " + request + "\ninglese : " + new_request)
+    image_files = Generate(new_request)
     col1, col2, col3 = st.columns(3)
     with col1:
         st.image(Image.open(BytesIO(base64.decodebytes(image_files[0].encode("utf-8")))))
