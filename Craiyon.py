9bff435ef9f12e9db0114e6a8c255dac846ee73d
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-01-13 10:32:45 +0100

    Update Craiyon.py

--- Craiyon.py
+++ Craiyon.py
@@ -12,9 +12,6 @@ def Generate(request):
 
 generator = Craiyon()
 st.title("🤖Genera Immagini uniche con l' I.A.📸")
-
-st.set_page_config(page_title="Genera Immagini uniche online by I.A. Italia", page_icon="📈")
-
 st.markdown("<center><h1>🤖Genera Immagini uniche con l'I.A.📸<small><br> Powered by INTELLIGENZAARTIFICIALEITALIA.NET </small></h1>", unsafe_allow_html=True)
 st.write('<p style="text-align: center;font-size:15px;" > <bold>Divertiti a generare immagini uniche e irrecreabili, il tutto gratis, online e utilizzando input direttamente in italiano.<bold>  </bold><p><br>', unsafe_allow_html=True)
 
@@ -26,7 +23,7 @@ hide_st_style = """
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-
+st.set_page_config(page_title="Genera Immagini uniche online by I.A. Italia", page_icon="📈")
 request = st.text_input("Sono in grado di disegnare tutto ciò che vuoi, se non ci credi provami🖌🤖","Dimmi solo cosa disegnare e lo  farò!")
 cola , colb, colc = st.columns(3)
 
